d972a5d1957ea6428011a7d02146e5fb6bf7482c
Kaizen Towfiq <kaizentowfiq@gmail.com>
2017-04-24 16:19:56 -0700

    tested that spotipy works in the django project

--- spotifyDashboard/login/views.py
+++ spotifyDashboard/login/views.py
@@ -3,7 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+import spotipy
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, World")
+	
+	lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+	
+	spotify = spotipy.Spotify()
+	results = spotify.artist_top_tracks(lz_uri)
+	return HttpResponse(results['tracks'][0]['name'])
