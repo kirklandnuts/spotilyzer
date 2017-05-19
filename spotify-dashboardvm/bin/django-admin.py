20e760ed942d05eee1a4595d0201333f993d01be
Kaizen Towfiq <kaizentowfiq@gmail.com>
2017-05-19 16:53:31 -0700

    organized git

--- spotify-dashboardvm/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/alexxu/Desktop/CS/spotifyFun/spotify-dashboard/spotify-dashboardvm/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
