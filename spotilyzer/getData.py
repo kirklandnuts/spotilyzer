906fc94b4f5ce6c752bfe54498f96052f9c83601
Kaizen Towfiq <kaizentowfiq@gmail.com>
2017-05-19 17:56:38 -0700

    songs table created correctly

--- spotilyzer/getData.py
+++ spotilyzer/getData.py
@@ -14,14 +14,25 @@ def test():
 def getSongs(songList, **kwargs):
 	#INPUT: takes a list of song id's, kwargs: accessToken, userID
 	#OUTPUT: returns a list
-	createDB(DBUSER, DBPASS)
+	createDB()
+	createSongsTable()
 
-def createDB(usr, pw):
-	con = connect(dbname='postgres', user=usr, host='localhost', password=pw)
+def createDB():
+	con = connect(dbname='postgres', user=DBUSER, host='localhost', password=DBPASS)
 	con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 	cur = con.cursor()
 	cur.execute('CREATE DATABASE ' + DBNAME)
 	cur.close()
 	con.close()
 
-createDB('spotilyzer', 'spotipass')
+def createSongsTable():
+	con = connect(dbname=DBNAME, user=DBUSER, host='localhost', password=DBPASS)
+	con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+	cur = con.cursor()
+	cur.execute("CREATE TABLE songs(songID char(22) PRIMARY KEY NOT NULL,artistIDs char(22) NOT NULL,albumID char(22) NOT NULL,song_title text NOT NULL,available_markets char(2)[],duration int,popularity int,danceability double precision,energy double precision,key int,loudness double precision,mode int,speechiness double precision,acousticness double precision,instrumentalness double precision,liveness double precision,valence double precision,tempo double precision,time_signature int)")
+	cur.close()
+	con.close()
+
+
+
+createSongsTable()
---
+++ spotilyzer/mockAnalysis.py
@@ -0,0 +1,3 @@
+import getData
+
+playlist = getSongs(["","",""])
