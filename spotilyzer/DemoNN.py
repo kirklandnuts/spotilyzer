466f25b4a428f85bc96fe0859ed02eb86b768aec
Kaizen Towfiq <kaizentowfiq@gmail.com>
2017-10-03 21:00:24 -0700

    stuff happened

--- spotilyzer/DemoNN.py
+++ spotilyzer/DemoNN.py
@@ -9,10 +9,13 @@ from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
+from sklearn.neural_network import MLPClassifier
 from matplotlib import style
 import itertools
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve # ROC Curves
@@ -27,12 +30,24 @@ def predictCategoryRF(training_set, test_set,  target, test_targert, componentsL
 	return test_set, classifier.predict(test_set[componentsList]), test_targert, classifier.score(test_set[componentsList], test_targert)
 
 def predictCategoryKNN(training_set, test_set,  target, test_targert, componentsList, k):
-    classifier = KNeighborsClassifier(n_neighbors=k, metric='minkowski')
-    classifier.fit(training_set[componentsList], target)
-    return test_set, classifier.predict(test_set[componentsList]), test_targert, classifier.score(test_set[componentsList], test_targert)
+	scaler = StandardScaler()
+	scaler.fit(training_set[componentsList])
+	training_set[componentsList] = scaler.transform(training_set[componentsList])
+	test_set[componentsList] = scaler.transform(test_set[componentsList])
+	classifier = KNeighborsClassifier(n_neighbors=k, metric='minkowski')
+	classifier.fit(training_set[componentsList], target)
+	return test_set, classifier.predict(test_set[componentsList]), test_targert, classifier.score(test_set[componentsList], test_targert)
 
+def predictCategoryNN(training_set, test_set,  target, test_targert, componentsList):
+	scaler = StandardScaler()
+	scaler.fit(training_set[componentsList])
+	training_set[componentsList] = scaler.transform(training_set[componentsList])
+	test_set[componentsList] = scaler.transform(test_set[componentsList])
+	mlp = MLPClassifier(hidden_layer_sizes=(13,13,13),max_iter=500)
+	mlp.fit(training_set[componentsList], target)
+	return test_set, mlp.predict(test_set[componentsList]), test_targert, mlp.score(test_set[componentsList], test_targert)
 
-categories = ['Jazz', 'Rock', 'Hip-Hop', 'Metal', 'Electronic/Dance', 'Pop', 'Indie']
+categories = ['Jazz', 'Rock']#, 'Hip-Hop', 'Metal', 'Electronic/Dance', 'Pop', 'Indie']
 allFeatures = ["popularity", "danceability", "energy", "key", "loudness", "speechiness", "acousticness",
 				 "instrumentalness", "liveness", "valence", "tempo", "time_signature"]
 
@@ -52,8 +67,8 @@ test_set = pd.concat(te_list)
 target = training_set['category']
 test_target = test_set['category']
 
-testdf, predictions, correctValues, score = predictCategoryRF(training_set, test_set, target, test_target, allFeatures, 5000)
-#testdf, predictions, correctValues, score = predictCategoryKNN(training_set, test_set, target, test_target, allFeatures, 83)
+#testdf, predictions, correctValues, score = predictCategoryNN(training_set, test_set, target, test_target, allFeatures)
+testdf, predictions, correctValues, score = predictCategoryKNN(training_set, test_set, target, test_target, allFeatures, 83)
 
 print(pd.crosstab(predictions, correctValues,
                   rownames=['Predicted Values'],
