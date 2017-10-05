54c4cb8b4b856e7a7ab9de2ac1e702367da8b359
Kaizen Towfiq <kaizentowfiq@gmail.com>
2017-10-04 21:54:49 -0700

    asdf

--- spotilyzer/Demo.py
+++ spotilyzer/Demo.py
@@ -9,6 +9,7 @@ import getData as gd
 
 
 SONGID = sys.argv[1]
+ACCESS_HEADER = gd.getAccessHeader()
 
 def songDataFrame(songid):
 	features = ["popularity", "danceability", "energy", "key", "loudness", "speechiness", "acousticness",\
@@ -20,11 +21,34 @@ def songDataFrame(songid):
 	df = pd.DataFrame(index=[0], data=pre_frame_d)
 	return df
 
-song_df = songDataFrame(SONGID)
+def getPlaylistSongIDs(access_header):
+    url = "GET https://api.spotify.com/v1/users/12139793063/playlists/21kX3cArKDRsEKpJOS7NBi/tracks"
+    resp = requests.get(url, headers=access_header)
+    songIDs = []
+    if resp.status_code == 200:
+        for i in resp.json()["tracks"]["items"]:
+            songIDs.append(i["track"]["id"])
+    return songIDs
+
+def printResults(df, clf):
+	classes = clf.classes_
+	probs = classifier.predict_proba(song_df)[0]
+	connected = {}
+	for i in list(range(0,len(classes))):
+		connected[probs[i]] = classes[i]
+	ordered_keys = sorted(connected.keys(), reverse=True)
+	count = 0
+	for k in ordered_keys:
+		count += 1
+		print("Prediction " + str(count) + ":"  + connected[k])
+
 fileObject = open(sys.argv[2],'rb')  
 classifier = pickle.load(fileObject)  
 
-prediction = classifier.predict(song_df)
+song_df = songDataFrame(SONGID)
 
-print("Prediction: " + prediction)
+song_ids = getPlaylistSongIDs(ACCESS_HEADER)
 
+import pdb
+pdb.set_trace()
+printResults(song_df, classifier)
