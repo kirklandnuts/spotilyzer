b0bd550e15c5997a080ec9c2ca751de5461324aa
Kaizen Towfiq <kaizentowfiq@gmail.com>
2017-10-11 22:05:56 -0700

    update

--- spotilyzer/jazz_or_not.py
+++ spotilyzer/jazz_or_not.py
@@ -1,14 +1,7 @@
-import pdb
-from sklearn.decomposition import PCA
-import requests
-import sys
 import pandas as pd
-import pickle
 import getData as gd
-#USAGE: python Demo.py songid model
-#input songid
-#output genre
-
+import sys
+import pickle
 
 SONGID = sys.argv[2]
 ACCESS_HEADER = gd.getAccessHeader()
@@ -35,43 +28,8 @@ def printResults(df, clf):
 		print("This is not Jazz")
 	print("="*25, "\n")
 
-
-def PCAOnDataFrame(df, training_set, features, components):
-	pca = PCA(n_components=components, random_state=7)
-	pca.fit(training_set[features])
-	components_col = [x for x in range(0, components)]
-	training_set_pca = pca.fit_transform(training_set[features])
-	sdf = pca.transform(df[features])
-	sdf = pd.DataFrame(sdf, columns=components_col)
-	return sdf
-
-
-categories = ['Jazz', 'Rock', 'Hip-Hop', 'Metal', 'Electronic/Dance', 'Pop']
-allFeatures = ["popularity", "danceability", "energy", "loudness", "speechiness", "acousticness",
-                 "instrumentalness", "liveness", "valence", "tempo"]
-
-test_set = pd.read_csv('jazz_te.csv')
-training_set = pd.read_csv('jazz_tr.csv')
-training_group = training_set.groupby(['category'])
-test_group = test_set.groupby(['category'])
-
-tr_list = []
-te_list = []
-
-for genre in categories:
-    tr_list.append(training_group.get_group(genre))
-    te_list.append(test_group.get_group(genre))
-
-training_set = pd.concat(tr_list)
-test_set = pd.concat(te_list)
-target = training_set['jazz']
-test_target = test_set['jazz']
-
-
-fileObject = open(sys.argv[1],'rb')
-classifier = pickle.load(fileObject)
-
-
-song_df = songDataFrame(SONGID)
-
-printResults(song_df, classifier)
+if __name__ == '__main__':
+	fileObject = open(sys.argv[1],'rb')
+	classifier = pickle.load(fileObject)
+	song_df = songDataFrame(SONGID)
+	printResults(song_df, classifier)
